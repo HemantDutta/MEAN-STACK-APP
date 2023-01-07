29e5e15c181c637a428590a78672d9bdfde25dc6
HemantDutta <hemantnorddutta2@gmail.com>
2023-01-07 14:44:29 +0530

    mysql connection made

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,4 @@
 const app = angular.module('myApp', []);
+app.controller('myCtrl', ($scope, $http)=>{
+
+})
--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3005;
+
+//Mysql connection
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "587548",
+    database: "mean"
+});
+
+conn.connect((err)=>{
+    if (err) throw err;
+    console.log('Connected');
+})
 
 //Init server
 app.listen(port, ()=>{
     console.log('Server is running on http://localhost:'+port);
-})
+});
+
+
+//get requests
+app.get("/", (req,res)=>{
+   let sql = "select * from users";
+   conn.query(sql, (err,rows)=>{
+      if (err) throw err;
+      res.send(rows);
+       console.log(rows);
+   });
+});
 
